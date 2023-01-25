2fc5f3d9b46335a002ccac14c4ad27fcdf0ad4e5
Torben Wetter <git@torben.id>
2023-01-25 02:15:10 +0100

    Add thesis template, remove examples

--- test/thesis/test.sh
+++ test/thesis/test.sh
@@ -4,7 +4,7 @@ source test-utils.sh
 
 # Template specific tests
 check "distro" lsb_release -c
-check "greeting" [ $(cat /usr/local/etc/greeting.txt | grep hey) ]
+check "thesis" [ $(cat /tmp/thesis.txt | grep result) ]
 
 # Report result
 reportResults
